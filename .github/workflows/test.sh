8ace19d88db62486c98fcddc149c39f13bb7514e
Cedric Brasey <cedric.brasey@plaet-side.co.uk>
2026-06-23 13:57:32 +0100

    Enable GitHub Actions workflow to push annotated tags for feature builds

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -6,8 +6,6 @@ BRN_NAME="origin/feature/lreg-123-asdfasdf"
  if [[ $BRN_NAME =~ feature/([a-z]+-[0-9]+)-.* ]]; then
   STORY_ID="${BASH_REMATCH[1]}"
   echo "STORY_ID=${STORY_ID}" >> "$GITHUB_OUTPUT"
-
-
   echo "STORY_ID found ${STORY_ID}"
 else
   echo "Branch does not have the correct name!" >&2
